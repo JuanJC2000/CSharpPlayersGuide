7fde46bd99c8097e546968dd4ca08886af86ef46
JuanJC2000 <juanjosejaimes@outlook.com>
2023-06-20 21:53:59 +0300

    same commit as last

---
+++ Watchtower/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Input an x and y value");
+int xValue = Convert.ToInt32(Console.ReadLine());
+int yValue = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (xValue == 0 && yValue == 0)
+    Console.WriteLine("The enemy is here!");
+
+else if (xValue > 0)
+    if (yValue > 0)
+        Console.WriteLine("The enemy is to the northeast!");
+    else if (yValue == 0)
+        Console.WriteLine("The enemy is to the east!");
+    else Console.WriteLine("The enemy is to the southeast!");
+
+else if (xValue < 0)
+    if (yValue > 0)
+        Console.WriteLine("The enemy is to the northwest!");
+    else if (yValue == 0)
+        Console.WriteLine("The enemy is to the west!");
+    else Console.WriteLine("The enemy is to the southwest!");
+
+else if (xValue == 0)
+    if (yValue > 0)
+        Console.WriteLine("The enemy is to the north!");
+    else if (yValue < 0)
+        Console.WriteLine("The enemy is to the south!");
+
