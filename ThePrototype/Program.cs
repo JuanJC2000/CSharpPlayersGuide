88d2c1f1f7448592b0c9423d52a6f824ea678899
JuanJC2000 <juanjosejaimes@outlook.com>
2023-06-21 16:43:18 +0300

    ThePrototype
    The Prototype, page 88, 100 exp. Tried to use switch statements instead of if and else but i couldnt wrap my head around it. This version is longer but gets the job done.

---
+++ ThePrototype/Program.cs
@@ -0,0 +1,52 @@
+﻿int chosenNumber;
+bool numberIsRight = false;
+
+do
+{
+    Console.Write("User 1, enter a number between 0 and 100: ");
+    chosenNumber = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+    
+
+}
+while (chosenNumber < 0 || chosenNumber > 100);
+
+Console.Clear();
+Console.WriteLine("User 2, guess the number.");
+
+do
+{
+    Console.Write("What is your next guess? ");
+    int whichNumber = Convert.ToInt32(Console.ReadLine());
+
+    if (whichNumber == chosenNumber)
+    {
+        Console.WriteLine("You guessed the number.");
+        break;
+    }
+       
+
+    else if (whichNumber > chosenNumber)
+        Console.WriteLine($"{whichNumber} is too high.");
+    else if (whichNumber < chosenNumber)
+        Console.WriteLine($"{whichNumber} is too low.");
+
+    /*
+    whichNumber = chosenNumber switch
+    {
+        whichNumber == chosenNumber => "You guessed the number.",
+        whichNumber > chosenNumber => $"{whichNumber} is too high.",
+        whichNumber < chosenNumber => $"{whichNumber} is too low."
+
+
+
+
+
+    };
+    */
+
+
+
+}
+while (true);
+
