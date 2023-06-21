8bf4da699301acee810ccbfa8fdd722e270e875b
JuanJC2000 <juanjosejaimes@outlook.com>
2023-06-21 16:59:53 +0300

    MagicCannon
    Using a for loop to go though 1-100, made the mistake of putting the conditonal with the most complexity at the bottom of the if loop. Should always be at the top since if there is a condition that applies before it, it will never run and thus this is a bug.

---
+++ TheMagicCannon/Program.cs
@@ -0,0 +1,28 @@
+﻿for(int x=1;  x <= 100; x++)
+{
+    //Console.WriteLine(x);
+    if (x % 3 == 0 && x % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"{x}: Fire and Electric");
+    }
+
+    else if (x % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{x}: Fire");
+    }
+    else if (x % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{x}: Electric");
+    }
+
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine($"{x}: Normal");
+    }
+
+    Console.ForegroundColor = ConsoleColor.White;
+}
